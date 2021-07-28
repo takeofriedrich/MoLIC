35d14ceb0c29b0d5b8d2fed6a7214950806e7629
Vinicius Takeo Friedrich Kuwaki <vtkwki@gmail.com>
2021-07-28 10:52:57 -0300

    initial commit

---
+++ codigo.py
@@ -0,0 +1,25 @@
+def leValor() -> int:
+    try:
+        x = int(input())
+        return x
+    except Exception:
+        return 0
+
+soma = 0
+
+while True:
+
+    print('Digite 0 para SAIR')
+    print('Digite 1 para SOMAR')
+
+    lido = input()
+
+    if lido == '0':
+        break
+
+    elif lido == '1':
+        print('Informe um inteiro:')
+        soma = soma + leValor()
+        print('Valor da soma atual = {}'.format(soma))
+
+print('Encerrando...')
